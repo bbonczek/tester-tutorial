116281543dd56c8b7d56713bf3a949bcf0b38444
Bartosz Baczek <bartosz.baczek@gmail.com>
2020-11-08 19:02:46 +0100

    added whole test project from angular course

---
+++ src/app/hero-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+  selector:    'app-hero-list',
+  templateUrl: './hero-list.component.html',
+  styles: [`
+    li {
+          cursor: pointer;
+    }
+  `],
+  providers:  [ HeroService ]
+})
+export class HeroListComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private service: HeroService) { }
+
+  ngOnInit() {
+    this.heroes = this.service.getHeroes();
+  }
+
+  selectHero(hero: Hero) { this.selectedHero = hero; }
+}
